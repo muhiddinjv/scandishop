9da7569bf5634337e16aaa25cb242114a61664a7
Emuallim <muhiddinjv@gmail.com>
2022-01-01 23:36:00 +0500

    feat: add hover effect to product card

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -4,14 +4,36 @@ import "../media/sass/Category.scss";
 import { NavLink } from "react-router-dom";
 import { ReactComponent as EmptyCart } from '../media/icons/empty-cart.svg';
 
+
 export default class Category extends Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+    this.state = {
+      isHovering: false
+    };
+  }
+
+  handleMouseOver() {
+    this.setState(() => ({
+      isHovering: true
+    }));
+  }
+
+  handleMouseOut() {
+    this.setState(() => ({
+      isHovering: false
+    }));
+  }
+
   generateProduct = () => {
     let products = this.props.category;
     // console.log("genProd",products);
     if (products) {
       return products.map((product) => {
         return (
-          <li className="category__product--card" key={product.id}>
+          <li className="category__product--card" key={product.id} onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
             <div className="category__product--image-wrapper">
               <img
                 className="category__product--image"
@@ -19,7 +41,7 @@ export default class Category extends Component {
                 alt={product.name}
               />   
               <NavLink className="category__product--btn" to="/product" >
-                <EmptyCart />
+                {this.state.isHovering && <EmptyCart />}
               </NavLink>
             </div>           
             <div className="category__product--body">
@@ -41,13 +63,15 @@ export default class Category extends Component {
 
   render() {
     return (
-      <div className="category">
+      <div className="category" >
         <div className="category__header">
           <h1 className="category__name">
             {this.props.category.map((x) => x.category)}
           </h1>
         </div>
         <ul className="category__product">{this.generateProduct()}</ul>
+        
+
       </div>
     );
   }

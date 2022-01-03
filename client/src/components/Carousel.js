58ce6f9be12d08faf7cf04806d0ff8d194d02b26
Emuallim <muhiddinjv@gmail.com>
2022-01-03 09:53:01 +0500

    fix: cycle carousel images onclick

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -2,29 +2,22 @@ import React, { Component } from "react";
 import "../media/sass/Carousel.scss";
 
 export default class Carousel extends Component {
-  render() {
-    return (
-      <div className="gallery">
-        <input type="radio" name="select" id="img-tab-1" defaultChecked/>
-        <label
-          htmlFor="img-tab-1"
-          style={{backgroundImage: "url(" + "https://images.unsplash.com/photo-1558981000-f294a6ed32b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjc5NjV9&auto=format&fit=crop&w=2550&q=80" + ")"}}
-        ></label>
-        <img
-          src="https://images.unsplash.com/photo-1558981000-f294a6ed32b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjc5NjV9&auto=format&fit=crop&w=2550&q=80"
-          border="0"
-        />
-
-        <input type="radio" name="select" id="img-tab-2" />
-        <label  
-          htmlFor="img-tab-2"
-          style={{backgroundImage: "url(" + "https://images.unsplash.com/photo-1558981359-219d6364c9c8?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" + ")"}}
-        ></label>
-        <img
-          src="https://images.unsplash.com/photo-1558981359-219d6364c9c8?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-          border="0"
-        />
-      </div>
-    );
+  carousel() {
+    let images = this.props.images[0];    
+    if (images) {
+      return images.map((img, ind) => {                  
+        return (
+            <div className="gallery product__carousel--img">
+              <input type="radio" name="select" id={`img-tab-${ind}`} defaultChecked={ind === 0}/>
+              <label htmlFor={`img-tab-${ind}`} style={{backgroundImage: `url(${img})`}} />
+              <img src={img} key={ind} border="0"/>
+            </div>
+        );
+      });
+    } else {
+      return <div className="loader"></div>;
+    }
   }
+
+  render() { return (this.carousel()) }
 }
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,34 +1,17 @@
 import React, { Component } from "react";
 import Carousel from "./Carousel";
+// import "../media/sass/Carousel.scss";
 
 
 
 export default class Product extends Component {
-    
-  carousel() {
-    let images = this.props.category;
-    // console.log(images);
-    
-    if (images) {
-      return images.map((img) => {          
-        return (
-            <div className="product__carousel--img">
-                {/* {img.gallery.map((im, ind)=><img src={im} alt={img.name} key={ind}/>)} */}
-            </div>
-        );
-      });
-    } else {
-      return <div className="loader"></div>;
-    }
-  }
+
   render() {
     return (
       <div className="product">
-        product page
         <div className="product__carousel">
-          <div className="product__carousel--imgs">{this.carousel()}</div>
+        <Carousel images={this.props.category.map(img=>img.gallery)} />
         </div>
-        <Carousel />
       </div>
     );
   }

73ed652616d3a224f8a8d698a15b5f805afb25dd
Emuallim <muhiddinjv@gmail.com>
2022-01-13 23:50:49 +0500

    feat: add attr action creator

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -1,5 +1,5 @@
 
-import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY } from './action-types/cart-actions'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_ATTRIBUTE } from './action-types/cart-actions'
 
 //add cart action
 export const addToCart= (id)=>{
@@ -26,6 +26,14 @@ export const subtractQuantity=(id)=>{
 export const addQuantity=(id)=>{
     return{
         type: ADD_QUANTITY,
+        id
+    }
+}
+
+//add attr action
+export const addAttribute=(id)=>{
+    return{
+        type: ADD_ATTRIBUTE,
         id
     }
 }

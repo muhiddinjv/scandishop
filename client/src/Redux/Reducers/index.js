c34b1603d103bb9c3d40a44f2a8338ebdac9b70d
Emuallim <muhiddinjv@gmail.com>
2022-03-18 23:54:11 +0500

    bug: cant add the same product with different attribute many times

--- client/src/Redux/Reducers/index.js
+++ client/src/Redux/Reducers/index.js
@@ -7,6 +7,7 @@ const initState = {
   currencies: [],
   addedItems: [],
   selCurrency: "USD",
+  selAttribute: []
 };
 
 const fetchData = () => {
@@ -48,6 +49,15 @@ const cartReducer = (state = initState, action) => {
   }
 
   if (action.type === "SELECT_CURRENCY") {
+    let findDeep = (data, category) => {
+      return data.some((e) => {
+        if (e.category === category) return true;
+        else if (e.attributes) return findDeep(e.attributes, category);
+      });
+    };
+    
+    console.log(findDeep(state.addedItems, "clothes"));
+
     return { ...state, selCurrency: action.currency };
   }
 
@@ -55,8 +65,11 @@ const cartReducer = (state = initState, action) => {
     const filtered = filterItem(action.id);
     const { addedItem } = filtered;
 
-    addedItem.attributes.filter((attr) =>
-      attr.name === action.name ? (attr.selected = action.attr) : null
+    console.log('action.attr :>> ', action.attr);
+
+    addedItem.attributes.filter(
+      // (attr) => attr.name === action.name && state.selAttribute.push(action.attr)
+      (attr) => attr.name === action.name && (attr.selected = action.attr)
     );
   }
 
@@ -66,71 +79,38 @@ const cartReducer = (state = initState, action) => {
     //check if the action id exists in the addedItems
 
     const existedItem = state.addedItems.find((item) => action.id === item.id);
-    // let attributeMatch = false;
+
+    let attributeMatch = false;
+    // let selectedAttribute = '';
+
     // for (const addItem of state.addedItems) {
     //   for (const attribute of addItem.attributes) {
     //     for (const item of attribute.items) {
     //       if (addItem.id === action.id && item.value === attribute.selected){
+    //         // selectedAttribute = attribute.selected;
     //         attributeMatch = true;
     //       }
     //     }
     //   }
     // }
-    // state.addedItems.filter(addItem=> {
-    //   return addItem.attributes.some(attr => console.log('attr', attr))
-    // })
 
-    /** 
-      * My method 
-      * * important info is highlighted
-      * ! Old method! do not use!
-      * ? should this method be exposed in the public API?
-      * TODO: refactor this method so that it conforms to the API
-      * @param myParam is the parameter for this method
-    **/
-   
-      //* important info
-      //! old method be careful
-      //? is it really necessary?
-      //TODO: refactor this part
-      //@param myParam 
-      //MORE THAN 3 SLASHES = ////CROSS OUT
+    const filterAttributes = (addedItem) => {
+      if (addedItem.id === action.id && addedItem.value === addedItem.selected){
+        attributeMatch = true;
+        return
+      }
+      state.addedItems.forEach(item => {
+        console.log('item :>> ', item);
+        // this is RECURSION baby!
+        filterAttributes(item)
+      })
+    }
 
-    // const posts = [
-    //   {
-    //     "categories": [
-    //       {
-    //         "title": "tag1"
-    //       },
-    //       {
-    //         "title": "tag2"
-    //       },
-    //       {
-    //         "title": "tag3"
-    //       }
-    //     ],
-    //     "title": "First post"
-    //   },
-    //   {
-    //     "categories": [
-    //       {
-    //         "title": "tag2"
-    //       },
-    //       {
-    //         "title": "tag3"
-    //       }
-    //     ],
-    //     "title": "Second Post"
-    //   }
-    // ];
-    
-    // const filter = "tag1";
-    
-    // const filtered = posts.filter(post => {
-    //   return post.categories.some(cat => cat.title === filter)
-    // });
+    filterAttributes(state.addedItems);
 
-    if (existedItem) {
+    console.log('attributeMatch :>> ', attributeMatch);
+
+    if (attributeMatch) {
       addedItem.quantity += 1;
       return {
         ...state,
@@ -139,6 +119,7 @@ const cartReducer = (state = initState, action) => {
     } else {
       addedItem.quantity = 1;
       const newTotal = state.total + price;
+      
       return {
         ...state,
         addedItems: [...state.addedItems, addedItem],
@@ -198,7 +179,6 @@ const cartReducer = (state = initState, action) => {
 
 export default cartReducer;
 
-
 // items: [
 //   {
 //     productId: 1,
@@ -216,3 +196,127 @@ export default cartReducer;
 //     ]
 //   }
 // ],
+
+// const treeData = {
+//   id: 1,
+//   title: "Group1",
+//   tagClass: "Object",
+//   children: [
+//       {
+//           id: 2,
+//           title: "Group2",
+//           tagClass: "Object",
+//           children: [
+//               { id: 3, title: "Tag1", tagClass: "Variable" },
+//               { id: 4, title: "Tag2", tagClass: "Variable" },
+//           ],
+//       },
+//       { id: 5, title: "Group3", tagClass: "Object" },
+//       { id: 6, title: "Tag3", tagClass: "Variable" },
+//   ],
+// };
+
+// console.log('treeData :>> ', treeData);
+
+// const recursiveFunc = (treeData) => {
+// if(treeData.children){
+//    //filter treeData.children
+//    const children = treeData.children.filter(child => {
+//     if(child.tagClass === 'Variable'){
+//       //if tagclass is variable we create tagIds property for treeData
+//       treeData.tagIds? treeData.tagIds.push(child.id) : treeData.tagIds = [child.id];
+//       // return false to filter out this child
+//       return false
+//     }
+//     //not varaible tagclass, we go deeper
+//     recursiveFunc(child);
+//     //keep the child
+//     return true
+//   })
+//   //if children is an empty array, delete children property from treeData
+//   children.length === 0 ? delete treeData.children : treeData.children = children
+// }
+// return treeData
+// };
+
+// const updatedTreeData = recursiveFunc(treeData);
+
+// console.log('updatedTreeData: ',updatedTreeData)
+
+const activityItems = [
+  { name: "Sunday", items: [{ name: "Gym", activity: "weights" }] },
+  {
+    name: "Monday",
+    items: [
+      { name: "Track", activity: "race" },
+      { name: "Work", activity: "meeting" },
+      {
+        name: "Swim",
+        items: [
+          { name: "Beach", activity: "scuba diving" },
+          { name: "Pool", activity: "back stroke" },
+        ],
+      },
+    ],
+  },
+];
+
+// let findDeep = (data, activity) => {
+//   return data.some((e) => {
+//     if (e.activity === activity) return true;
+//     else if (e.items) return findDeep(e.items, activity);
+//   });
+// };
+
+// console.log(findDeep(activityItems, "scuba diving"));
+
+
+ /**
+     * My method
+     * * important info is highlighted
+     * ! Old method! do not use!
+     * ? should this method be exposed in the public API?
+     * TODO: refactor this method so that it conforms to the API
+     * @param myParam is the parameter for this method
+     **/
+
+    //* important info
+    //! old method be careful
+    //? is it really necessary?
+    //TODO: refactor this part
+    //@param myParam
+    //MORE THAN 3 SLASHES = ////CROSS OUT
+
+    // const posts = [
+    //   {
+    //     "categories": [
+    //       {
+    //         "title": "tag1"
+    //       },
+    //       {
+    //         "title": "tag2"
+    //       },
+    //       {
+    //         "title": "tag3"
+    //       }
+    //     ],
+    //     "title": "First post"
+    //   },
+    //   {
+    //     "categories": [
+    //       {
+    //         "title": "tag2"
+    //       },
+    //       {
+    //         "title": "tag3"
+    //       }
+    //     ],
+    //     "title": "Second Post"
+    //   }
+    // ];
+
+    // const filter = "tag1";
+
+    // const filtered = posts.filter(post => {
+    //   return post.categories.some(cat => cat.title === filter)
+    // });

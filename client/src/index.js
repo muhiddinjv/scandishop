b01cf8d1801c324c8aec62f839c468108046b58c
Emuallim <muhiddinjv@gmail.com>
2022-04-21 19:10:12 +0500

    feat: store cart items in localstorage with redux-persist

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
-import { BrowserRouter } from 'react-router-dom';
-import cartReducer from './Redux/Reducers';
-import {Provider} from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/App";
+import { BrowserRouter } from "react-router-dom";
+import cartReducer from "./Redux/Reducers";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import { createStore } from "redux";
 
-const store = createStore(cartReducer);
+const persistConfig = {
+  key: "persist-key",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartReducer);
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+//ef2b0fa head10

826326f7e6026cd6b6221d66907158faebd72e5d
Emuallim <muhiddinjv@gmail.com>
2022-04-22 18:09:38 +0500

    play around with redux-persist

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
 import { BrowserRouter } from "react-router-dom";
-import cartReducer from "./Redux/Reducers";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import cartReducer from "./Redux/Reducers";
 
 const store = createStore(cartReducer);
 

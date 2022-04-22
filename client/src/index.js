b9165533e3f9759cd5c949c66a20043fc34658df
Emuallim <muhiddinjv@gmail.com>
2022-04-22 14:48:46 +0500

    refactor: remove redux-persist

--- client/src/index.js
+++ client/src/index.js
@@ -4,29 +4,15 @@ import App from "./App/App";
 import { BrowserRouter } from "react-router-dom";
 import cartReducer from "./Redux/Reducers";
 import { Provider } from "react-redux";
-import { persistStore, persistReducer } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import storage from "redux-persist/lib/storage";
 import { createStore } from "redux";
 
-const persistConfig = {
-  key: "persist-key",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, cartReducer);
-const store = createStore(persistedReducer);
-const persistor = persistStore(store);
+const store = createStore(cartReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
-
-//ef2b0fa head10
